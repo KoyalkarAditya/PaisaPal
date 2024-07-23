f3970f7106e88275e8eaf32209252aabdf05db16
KoyalkarAditya <koyalkaraditya123@gmail.com>
2024-07-23 20:17:57 +0530

    fixed th bug

--- packages/db/prisma/seed.ts
+++ packages/db/prisma/seed.ts
@@ -2,43 +2,42 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  const alice = await prisma.user.upsert({
-    where: { number: "9999999999" },
+  const a = await prisma.user.upsert({
+    where: { number: "22222222" },
     update: {},
     create: {
-      number: "9999999999",
-      password: "alice",
-      name: "alice",
+      number: "22222222",
+      password: "22222222",
+      name: "Jack",
       OnRampTransaction: {
         create: {
           startTime: new Date(),
           status: "Success",
           amount: 20000,
-          token: "122",
+          token: "100",
           provider: "HDFC Bank",
         },
       },
     },
   });
-  const bob = await prisma.user.upsert({
-    where: { number: "9999999998" },
+  const b = await prisma.user.upsert({
+    where: { number: "111111111" },
     update: {},
     create: {
-      number: "9999999998",
-      password: "bob",
-      name: "bob",
+      number: "111111111",
+      password: "111111111",
+      name: "Will",
       OnRampTransaction: {
         create: {
           startTime: new Date(),
           status: "Failure",
-          amount: 2000,
-          token: "123",
+          amount: 200000,
+          token: "101",
           provider: "HDFC Bank",
         },
       },
     },
   });
-  console.log({ alice, bob });
 }
 main()
   .then(async () => {
